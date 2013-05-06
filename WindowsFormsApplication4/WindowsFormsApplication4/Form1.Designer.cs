242fa8141d617e6266b57fb2ee083c32481fbef8
Robert Winkler <robert.winkler@wp.pl>
2013-05-06 07:07:15 +0200

    dodany przyklad dla 2 naroznikow

---
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication4
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cornerA1 = new System.Windows.Forms.PictureBox();
+			this.cornerA2 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.cornerA1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.cornerA2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// cornerA1
+			// 
+			this.cornerA1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.cornerA1.Location = new System.Drawing.Point(53, 39);
+			this.cornerA1.Margin = new System.Windows.Forms.Padding(0);
+			this.cornerA1.Name = "cornerA1";
+			this.cornerA1.Size = new System.Drawing.Size(9, 9);
+			this.cornerA1.TabIndex = 0;
+			this.cornerA1.TabStop = false;
+			this.cornerA1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cornerA1_MouseDown);
+			this.cornerA1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.cornerA1_MouseMove);
+			this.cornerA1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.cornerA1_MouseUp);
+			// 
+			// cornerA2
+			// 
+			this.cornerA2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.cornerA2.Location = new System.Drawing.Point(53, 155);
+			this.cornerA2.Margin = new System.Windows.Forms.Padding(0);
+			this.cornerA2.Name = "cornerA2";
+			this.cornerA2.Size = new System.Drawing.Size(10, 10);
+			this.cornerA2.TabIndex = 1;
+			this.cornerA2.TabStop = false;
+			this.cornerA2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cornerA2_MouseDown);
+			this.cornerA2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.cornerA2_MouseMove);
+			this.cornerA2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.cornerA2_MouseUp);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.cornerA2);
+			this.Controls.Add(this.cornerA1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.cornerA1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.cornerA2)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox cornerA1;
+		private System.Windows.Forms.PictureBox cornerA2;
+	}
+}
+
---
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		int AreaLeft
+		{
+			set
+			{
+				cornerA1.Left = value;
+				cornerA2.Left = value;
+			}
+			get { return cornerA1.Left; }
+		}
+
+		int AreaTop
+		{
+			set { cornerA1.Top = value; }
+			get { return cornerA1.Top; }
+		}
+
+		int AreaBottom
+		{
+			set { cornerA2.Top = value; }
+			get { return cornerA2.Top; }
+		}
+
+		int AreaRight
+		{
+			set { ; }
+			// tbd
+			get { return 0; }
+		}
+
+		bool processA1 = false;
+		Point locationA1;
+
+		private void cornerA1_MouseDown(object sender, MouseEventArgs e)
+		{
+			processA1 = true;
+			locationA1 = cornerA1.PointToScreen(e.Location);
+		}
+
+		private void cornerA1_MouseUp(object sender, MouseEventArgs e)
+		{
+			processA1 = false;
+		}
+
+		private void cornerA1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (processA1)
+			{
+				Point oldLocation = locationA1;
+				locationA1 = cornerA1.PointToScreen(e.Location);
+
+				//using (var gfx = CreateGraphics())
+				//{
+				//	Debug.WriteLine("X:{0}; Y:{1}; dX:{2}; dY:{3}; DpiX:{4}; DpiY:{5}", 
+				//		e.Location.X, e.Location.Y, 
+				//		locationA1.X - oldLocation.X, locationA1.Y - oldLocation.Y, 
+				//		gfx.DpiX, gfx.DpiY);
+				//}
+
+				AreaLeft += locationA1.X - oldLocation.X;
+				AreaTop += locationA1.Y - oldLocation.Y;
+			}
+		}
+
+		bool processA2 = false;
+		Point locationA2;
+
+		private void cornerA2_MouseDown(object sender, MouseEventArgs e)
+		{
+			processA2 = true;
+			locationA2 = cornerA2.PointToScreen(e.Location);
+		}
+
+		private void cornerA2_MouseUp(object sender, MouseEventArgs e)
+		{
+			processA2 = false;
+		}
+
+		private void cornerA2_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (processA2)
+			{
+				Point oldLocation = locationA2;
+				locationA2 = cornerA2.PointToScreen(e.Location);
+
+				AreaLeft += locationA2.X - oldLocation.X;
+				AreaBottom += locationA2.Y - oldLocation.Y;
+			}
+		}
+	}
+}
